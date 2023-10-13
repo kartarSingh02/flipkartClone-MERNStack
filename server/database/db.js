96129b7eb945d5ed28dcbe6a8b30863ca5d582e2
Kartar Singh <k4kartar4520@gmail.com>
2023-10-13 17:22:12 +0530

    added server

---
+++ server/database/db.js
@@ -0,0 +1,13 @@
+
+
+export const Connection = () =>{
+
+    try{
+
+    }
+    catch(error){
+        console.log("Error while connecting to backend",error.message)
+    }
+}
+
+export default Connection
---
+++ server/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import Connection from './database/db.js';
+const app = express();
+
+const PORT = 8000;
+
+Connection();
+app.listen(PORT, () => console.log(`Server running on port ${PORT} `))
