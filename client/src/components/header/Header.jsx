6048984e2f5cc076143d8292af1443216afba514
Kartar Singh <k4kartar4520@gmail.com>
2023-10-13 13:48:37 +0530

    added components

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import {AppBar,Toolbar,Box, styled, Typography} from '@mui/material'
+
+// Components import 
+import Search from './Search';
+import CustomButtons from './CustomButtons';
+
+// This is to use to style the material UI, basically Override and where we want to use this style replace that MateiralUI into the name which we have given
+const StyledHeader = styled(AppBar)`
+    background: #2872f0;
+    height:55px;
+`
+
+const Component = styled(Box)`
+    margin-left: 12%;
+    line-height:0;
+`
+
+const SubHeading = styled(Typography)`
+    font-size: 10px;
+    font-style: italic;
+`
+
+
+// if we have to style the html tag then we have to put into '' and styling will be like objects ({})
+const PlusImage=styled('img')({
+    height: 10,
+    width: 10,
+    marginLeft: 4
+})
+
+const CustomButtonWrapper = styled(Box)`
+    margin:0 5% 0 auto;
+`
+
+function Header() {
+const logoURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/flipkart-plus_8d85f4.png';
+const subURL = 'https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/plus_aef861.png';
+  return (
+    <div>
+      <StyledHeader>
+        <Toolbar style={{minHeight:55}}>
+        <Component>
+            <img src={logoURL} alt="logo" style={{width:75}} />
+            <Box style={{display:'flex'}}>
+                <SubHeading>Explore&nbsp;
+                    <Box component="span" style={{color: '#FFE500'}}>Plus</Box>
+                </SubHeading>
+                <PlusImage src={subURL} alt='sub-logo'/>
+            </Box>
+            
+        </Component>
+        {/* search component */}
+        <Search/>
+        {/* Button Component */}
+        <CustomButtonWrapper>
+            <CustomButtons/>
+        </CustomButtonWrapper>
+        </Toolbar>
+      </StyledHeader>
+    </div>
+  )
+}
+
+export default Header
---
+++ client/src/components/header/Search.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { InputBase,Box,styled } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+
+const SearchContainer = styled(Box)`
+    background: #fff;
+    width:38%;
+    border-radius:3px;
+    margin-left:10px;
+    display:flex;
+    
+`
+const InputSearchBase = styled(InputBase)`
+    padding-left:20px;
+    width:100%;
+`
+
+const SearchBarWrapper = styled(SearchIcon)`
+    color:blue;
+    padding:5px;
+
+`
+
+function Search() {
+  return (
+      <SearchContainer>
+        <InputSearchBase placeholder='Search for products, brands and more'/>
+        <SearchBarWrapper/>
+      </SearchContainer>
+  )
+}
+
+export default Search
